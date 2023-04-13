850e7ba0df2f188674693ec62394e393883b59be
Angelina <linaemalmros@gmail.com>
2023-04-13 12:38:42 +0200

    börjat pilla med onclick på ikonerna

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,26 +1,39 @@
-import Fireworks from "../Fireworks/Fireworks"
 import rock from '../../assets/rock.png'
 import paper from '../../assets/paper.png'
 import scissors from '../../assets/scissors.png'
 import './game.css'
+import { useState } from 'react'
 
 const Game = () => {
+
+    const [sign, setSign] = useState('')
+
+    // metod för fetch sign
+    console.log(sign);
+
 
     return (
         <>
             {/* ska köras vid vinst */}
-            <Fireworks /> 
-            <div id="icons">
-                <img id="rock" src={rock}></img>
-                <img id="paper" src={paper}></img>
-                <img id="scissors" src={scissors}></img>
-            </div>
-
-            <div id="choice">
-                <p>ROCK PAPER SCISSORS</p>
+            {/* <Fireworks /> */}
+            <div className="icons">
+                <img id="rock" src={rock} onClick={() => setSign('rock')}></img>
+                <img id="paper" src={paper} onClick={() => setSign('paper')}></img>
+                <img id="scissors" src={scissors} onClick={() => setSign('scissors')}></img>
             </div>
         </>
     )
+}
+
+const Fireworks = () => {
+    return (
+        <>
+            <div className="pyro">
+                <div className="before"></div>
+                <div className="after"></div>
+            </div>
+        </>
+    );
 }
 
 export default Game
