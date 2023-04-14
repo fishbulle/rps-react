0e8802443f5fa9db22aee254097c57647fcf2fd0
Angelina <linaemalmros@gmail.com>
2023-04-14 13:41:53 +0200

    skapat custom hooks för fetch requests

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,16 @@
+export interface Game {
+    gameId: number
+    playerOne: { 
+        playerId: number, 
+        username: string 
+    }
+    playerOneMove: string
+    playerTwo: { 
+        playerId: number, 
+        username: string 
+    }
+    playerTwoMove: string
+    status: string
+    result: string
+}
+
