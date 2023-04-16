23268dd88750c2650f8cfac9dc9ba987aefa6d33
Angelina <linaemalmros@gmail.com>
2023-04-16 11:21:42 +0200

    en kommentar till

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,6 +1,9 @@
 import cats from '../assets/cats.png'
 import { Link } from "react-router-dom";
 
+// fetch POST startGame
+// lägga till onClick på Link to game (så nytt spel startas)
+
 const Homepage = () => (
     <div className="menu-div">
         <h1>What would you like to do?</h1>
